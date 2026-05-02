65eba65cb5dfefd899c6c3473f463a0fc1d3d0cb
Рашитов Эдуард Ринатович <rashitov@skbkontur.ru>
2026-05-02 23:18:53 +0500

    fix: поддержка протокол

--- apps/products/repositories/product_repository.py
+++ apps/products/repositories/product_repository.py
@@ -1,12 +1,6 @@
-from apps.products.models import Product, ProductCharacteristic, ProductImage
+from apps.products.models import Product
 from apps.products.schemas.product import (
-    ProductCharacteristicCreateSchema,
-    ProductCharacteristicReadSchema,
-    ProductCharacteristicUpdateSchema,
     ProductCreateSchema,
-    ProductImageCreateSchema,
-    ProductImageReadSchema,
-    ProductImageUpdateSchema,
     ProductReadSchema,
     ProductUpdateSchema,
 )
@@ -14,21 +8,4 @@ from db import DBCrudRepository
 
 
 class ProductRepository(DBCrudRepository[Product, ProductCreateSchema, ProductReadSchema, ProductUpdateSchema]):
-    pass
-
-
-class ProductImageRepository(
-    DBCrudRepository[ProductImage, ProductImageCreateSchema, ProductImageReadSchema, ProductImageUpdateSchema]
-):
-    pass
-
-
-class ProductCharacteristicRepository(
-    DBCrudRepository[
-        ProductCharacteristic,
-        ProductCharacteristicCreateSchema,
-        ProductCharacteristicReadSchema,
-        ProductCharacteristicUpdateSchema,
-    ]
-):
     pass
