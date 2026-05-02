9be1eb4fdff0a0c82d1c56372a7740eb7b5e6345
Моховиков Максим Дмитриевич <mokhovikov@tochka.com>
2026-05-02 17:53:06 +0500

    feat: добавил провайдеры

--- apps/depends.py
+++ apps/depends.py
@@ -1,1 +1,6 @@
-providers = []
+from .auth.depends import provider as auth_provider
+from .products.depends import provider as products_provider
+from .skus.depends import provider as skus_provider
+
+
+providers = [auth_provider, products_provider, skus_provider]
